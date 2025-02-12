233d41d9473ed16c022a49559257b7e5d23d2b5c
Maksim Kalinin <kalinin.m.a@bk.ru>
2025-02-12 22:54:07 +0300

    Add generateMetadata to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,32 @@ import Clients from "@/components/Clients";
 import Awards from "@/components/Awards";
 import Technologies from "@/components/Technologies";
 import Form from "@/components/Form";
+import { notFound } from "next/navigation";
+import { Metadata } from "next";
 
-export default function Home() {
+async function getHomePage() {
+	const res = await fetch("http://5693366a1538.vps.myjino.ru:49506/main", {
+		cache: "force-cache",
+	});
+	const data = await res.json();
+	if (!data) notFound();
+	return data;
+}
+
+export async function generateMetadata(): Promise<Metadata> {
+	const { seo } = await getHomePage();
+
+	return {
+		title: seo.title,
+		description: seo.description,
+		keywords: seo.keywords,
+		openGraph: {
+			url: new URL(seo.image, process.env.API_URL),
+		},
+	};
+}
+
+export default async function Home() {
 	return (
 		<>
 			<div>
